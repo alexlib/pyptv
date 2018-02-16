0aee8c6347db8f68c285b7e933df8e9dd510456a
Alex Liberzon <alex.liberzon@gmail.com>
2018-02-16 10:38:23 +0100

    add path also to the script

--- install_openptv.sh
+++ install_openptv.sh
@@ -10,5 +10,8 @@ sudo make install
 cd ../../py_bind
 python setup.py build_ext -I/usr/local/include -L/usr/local/lib
 python setup.py install
+export PATH=$/usr/local/lib:$PATH
+export LD_LIBRARY_PATH=$/usr/local/lib:$LD_LIBRARY_PATH  
+python -c "import sys; print sys.path"
 cd test
 nosetests -v
