2f3ccf77bd53aa301898b5c2e54ddd3d6196518d
Alex Liberzon <alex.liberzon@gmail.com>
2018-02-16 11:31:21 +0100

    typo

--- install_openptv.sh
+++ install_openptv.sh
@@ -11,7 +11,7 @@ cd ../../py_bind
 python setup.py build_ext -I/usr/local/include -L/usr/local/lib
 python setup.py install
 export PATH=$PATH:/usr/local/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 echo $LD_LIBRARY_PATH
 echo $PATH
 python -c "import sys; print sys.path"
