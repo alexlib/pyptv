3f900134eb24c7d2566a2d436bc3e4bf625a83f4
Alex Liberzon <alex.liberzon@gmail.com>
2018-02-16 12:48:25 +0100

    nosetests work locally inside /tests

--- tests/test_pyptv_batch.py
+++ tests/test_pyptv_batch.py
@@ -2,4 +2,4 @@ from pyptv import pyptv_batch
 
 def test_pyptv_batch():
     # assert cli.cli() == 'CLI template'
-    pyptv_batch.main('tests/test_cavity', 10000, 10004)
+    pyptv_batch.main('./test_cavity', 10000, 10004)
