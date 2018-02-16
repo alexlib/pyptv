1daab1b11618a104c2e1502a9bfd5ef7605340e5
Alex Liberzon <alex.liberzon@gmail.com>
2018-02-16 11:46:01 +0100

    DYLD_LIBRARY_PATH

--- install_openptv.sh
+++ install_openptv.sh
@@ -12,6 +12,7 @@ python setup.py build_ext -I/usr/local/include -L/usr/local/lib
 python setup.py install
 export PATH=$PATH:/usr/local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib
 echo $LD_LIBRARY_PATH
 echo $PATH
 python -c "import sys; print sys.path"
