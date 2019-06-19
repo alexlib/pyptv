1a985d282a70b553e6e8bc7a0ba0d8546bbeba14
Alex Liberzon <alexlib@users.noreply.github.com>
2019-06-19 23:58:33 +0300

    fixed two bugs, version 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = []
 
 setup(
     name='pyptv',
-    version='0.1.1b',
+    version='0.1.2',
     description='Python GUI for the OpenPTV library `liboptv`',
     author="Alex Liberzon",
     author_email='alex.liberzon@gmail.com',
