95c0ab14d69453676711c73e08ed017875a35a67
Itay Zandbank <itay@chelem.co.il>
2019-06-22 01:02:52 +0300

    Fix imports - relative when needed. Bump version number.

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
 from setuptools import setup
 
-requirements = []
+requirements = [
+    'numpy==1.16.4',
+    'certifi==2019.6.16',
+    'chaco==4.7.2',
+    'cloudpickle==1.2.1',
+    'dask==1.2.2',
+    'decorator==4.4.0',
+    'enable==4.7.2',
+    'fonttools==3.43.1',
+    'future==0.17.1',
+    'imageio==2.5.0',
+    'libtiff==0.4.2',
+    'networkx==2.3',
+    'optv==0.2.6',
+    'Pillow==6.0.0',
+    'pyface==6.1.1',
+    'Pygments==2.4.2',
+    'PyQt4==4.11.4',
+    'PyWavelets==1.0.3',
+    'pyxdg==0.26',
+    'PyYAML==5.1.1',
+    'scikit-image==0.15.0',
+    'scipy==1.3.0',
+    'six==1.12.0',
+    'toolz==0.9.0',
+    'traits==5.1.1',
+    'traitsui==6.1.1',
+    'wincertstore==0.2',
+]
 
 setup(
     name='pyptv',
-    version='0.1.2',
+    version='0.1.3',
     description='Python GUI for the OpenPTV library `liboptv`',
     author="Alex Liberzon",
     author_email='alex.liberzon@gmail.com',
@@ -12,8 +40,7 @@ setup(
     packages=['pyptv'],
     entry_points={
         'console_scripts': [
-           	'pyptv=pyptv.cli:cli',
-		'ptvgui=pyptv.pyptv_gui:main'
+    		'pyptv=pyptv.pyptv_gui:main'
         ]
     },
     install_requires=requirements,
