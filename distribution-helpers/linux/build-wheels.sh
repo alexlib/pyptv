9b2b0e4b7497de2092812894b2b0760b7152d993
Daniella <daniellaillouz@gmail.com>
2019-10-30 11:14:37 +0200

    update versions for enable and chaco in build-wheels.sh

--- distribution-helpers/linux/build-wheels.sh
+++ distribution-helpers/linux/build-wheels.sh
@@ -9,12 +9,12 @@ cd /src
 # Enable 4.7.2 uses old Cython, which doesn't support Python 3.7. They have a fix on master, but it wasn't released yet
 # Hopefully a future version of enable will include this fix and we'll just be able to use it
 # For now, we check out the entire repository, use the 4.7.2 tag but patch the file from its own commit
-git clone https://github.com/enthought/enable.git --branch 4.7.2
+git clone https://github.com/enthought/enable.git --branch 4.8.1
 
 cd enable
 
 # Get the correct kiva/_cython_speedups.cpp
-git checkout 969c973 -- kiva/_cython_speedups.cpp
+#git checkout 969c973 -- kiva/_cython_speedups.cpp
 
 /opt/python/cp37-cp37m/bin/pip install numpy
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
@@ -28,10 +28,10 @@ cp dist/* /wheels
 # Chaco has the same problem as enable, in the file downsample/_lttb.c
 # We fix it the same way, by getting just this file from the commit that fixed the issue
 cd /src
-git clone https://github.com/enthought/chaco.git --branch 4.7.2
+git clone https://github.com/enthought/chaco.git --branch 4.8.0
 
 cd chaco
-git checkout 14c5539 -- chaco/downsample/_lttb.c
+#git checkout 14c5539 -- chaco/downsample/_lttb.c
 
 /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
@@ -39,7 +39,7 @@ cp dist/* /wheels
 
 # Traits
 cd /src
-git clone https://github.com/enthought/traits.git --branch 5.1.1 --single-branch --depth 1
+git clone https://github.com/enthought/traits.git --branch 5.1.2 --single-branch --depth 1
 cd traits
 /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
