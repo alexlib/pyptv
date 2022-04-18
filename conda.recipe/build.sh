36d5575ceeb1946cad11d6e9bd07ea69213c82a3
Alex Liberzon <alexlib@users.noreply.github.com>
2022-04-18 21:44:20 +0300

    Create build.sh

---
+++ conda.recipe/build.sh
@@ -0,0 +1,6 @@
+conda create -n pyptv_py39 python=3.9 -y
+conda activate pyptv_py39
+conda install swig pyyaml -y
+pip install git+https://github.com/enthought/enable
+pip install optv --index-url https://pypi.fury.io/pyptv
+pip install pyptv
