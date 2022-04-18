d5e64d61f2eb4158c43bbc28b5f62850dd4bf596
Alex Liberzon <alexlib@users.noreply.github.com>
2022-04-18 21:53:49 +0300

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,8 +1,3 @@
-conda create -n pyptv_py39 python=3.9 -y
-conda activate pyptv_py39
-conda install swig pyyaml -y
-pip install git+https://github.com/enthought/enable
-pip install optv --index-url https://pypi.fury.io/pyptv
-pip install pyptv
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+
+{{ PYTHON }} -m pip install . --no-deps -vv --index-url https://pypi.fury.io/pyptv --extra-index-url https://pypi.org/simple
 
