c169091d1d3b57e6dbed73832f10745c73e93542
Alex Liberzon <alexlib@users.noreply.github.com>
2022-04-19 10:08:45 +0300

    tried another meta.yaml - it builds but does not work afterwards

--- conda.recipe/bld.bat
+++
@@ -1,2 +0,0 @@
-"%PYTHON%" -m pip install . --no-deps -vv --index-url https://pypi.fury.io/pyptv --extra-index-url https://pypi.org/simple
-if errorlevel 1 exit 1
--- conda.recipe/build.sh
+++
@@ -1,3 +0,0 @@
-
-{{ PYTHON }} -m pip install . --no-deps -vv --index-url https://pypi.fury.io/pyptv --extra-index-url https://pypi.org/simple
-
