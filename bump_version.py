0ec72ed47501ce2ef4307b71226c9a54534fe0ae
Alex Liberzon <alexlib@users.noreply.github.com>
2024-12-20 16:41:59 +0200

    0.2.8

--- bump_version.py
+++ bump_version.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # Define the current version
-CURRENT_VERSION = "0.2.9"
+CURRENT_VERSION = "0.2.8"
 
 def bump_version(version):
     """Increment the last integer of the version string by 1."""
@@ -21,22 +21,33 @@ def update_version_in_file(file_path, old_version, new_version):
     with open(file_path, 'w') as file:
         file.write(updated_content)
 
+    return updated_content != content
+
 def search_and_update_versions(directory, old_version, new_version):
     """Search for all files in the directory and update the version number."""
+    changed_files = []
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.py') or file.endswith('.yaml'):
                 file_path = Path(root) / file
-                update_version_in_file(file_path, old_version, new_version)
+                if update_version_in_file(file_path, old_version, new_version):
+                    changed_files.append(file_path)
+    return changed_files
 
 if __name__ == '__main__':
     # Define the directory to search for version updates
-    directory_to_search = Path('/home/user/Documents/repos/pyptv/')
+    directory_to_search = Path('.')
 
     # Calculate the new version
     new_version = bump_version(CURRENT_VERSION)
 
     # Search and update versions in files
-    search_and_update_versions(directory_to_search, CURRENT_VERSION, new_version)
+    changed_files = search_and_update_versions(directory_to_search, CURRENT_VERSION, new_version)
 
-    print(f"Version updated from {CURRENT_VERSION} to {new_version}")
+    # Print the changed files
+    if changed_files:
+        print("Updated version in the following files:")
+        for file_path in changed_files:
+            print(file_path)
+    else:
+        print("No files were updated.")
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyptv',
-    version='0.2.7',
+    version='0.2.8',
     packages=find_packages(include=['pyptv', 'openptv']),
 )
